1928479785778223cfc5c3b2078beee041f3fc15
S0rrow <jae9hell@gmail.com>
2018-05-18 21:49:42 +0900

    chatcounter modified

--- src/edu/handong/csee/java/chatcounter/ChatCounter.java
+++ src/edu/handong/csee/java/chatcounter/ChatCounter.java
@@ -1,5 +1,7 @@
 package edu.handong.csee.java.chatcounter;
 
+import java.util.Scanner;
+
 public class ChatCounter {
 	static void main(String[] args) {
 		ChatCounter actor = new ChatCounter();
@@ -7,6 +9,13 @@ public class ChatCounter {
 	}
 	
 	private void acto() {
-		
+		Scanner keyboard = new Scanner(System.in);
+		FileLoader file = new FileLoader();
+		System.out.println("Type in your name of file:");
+		String fileName = keyboard.nextLine();
+		file.ScanFile(fileName);
+		String data = file.getData();
+		System.out.println(data);
+		keyboard.close();
 	}
 }
